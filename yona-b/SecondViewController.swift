cf54858ff9b4ba578b4473515c5c249822668a1e
株式会社YONA <yona@kabushikikaishaYONAnoMacBook-Pro.local>
2017-06-28 17:46:54 +0900

    Initial Commit

---
+++ yona-b/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  yona-b
+//
+//  Created by 株式会社YONA on 2017/06/28.
+//  Copyright © 2017年 株式会社YONA. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ yona-bTests/yona_bTests.swift
@@ -0,0 +1,36 @@
+//
+//  yona_bTests.swift
+//  yona-bTests
+//
+//  Created by 株式会社YONA on 2017/06/28.
+//  Copyright © 2017年 株式会社YONA. All rights reserved.
+//
+
+import XCTest
+@testable import yona_b
+
+class yona_bTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
